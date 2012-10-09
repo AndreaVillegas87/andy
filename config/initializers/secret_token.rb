6ef92b1217278f1629d019adf75fe9f80dbc5c23
Lunes <andrea.email@example.com>
2012-10-08 17:04:00 -0700

    Commit de inicio

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Andy::Application.config.secret_token = 'a2e5b087224b2dee3b9ec2f37a4ed6f4941510b62c66b2ef0180008f393e598be58a63ef666fd219b503a4b5ddda4700d3f6ebbd43907c187142c7bbe2972def'
